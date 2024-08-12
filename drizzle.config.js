66022a9e3499373f78323c9a726a548b61d5b866
unknown <jinojino122@gmai.com>
2024-08-12 15:45:40 +0530

    auth commit

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,9 +2,9 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'mysql',
     dbCredentials: {
-        host: "localhost",
-        user: "root",
-        database: "doutya",
-        password: "jinoJINO"
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        database: process.env.DATABASE_NAME,
+        password: process.env.DATABASE_PASSWORD
     }
   };
--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,22 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "doutya",
-  password: "jinoJINO"
+// const connection = await mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   database: "doutya",
+//   password: "jinoJINO"
+// });
+// https://247.163.178.68.host.secureserver.net:2083/
+
+const connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
 });
+
 
 export const db = drizzle(connection);
 
