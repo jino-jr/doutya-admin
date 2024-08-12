45de6d56c03cb01e51406cbc2db134f1d49d2445
unknown <jinojino122@gmai.com>
2024-08-12 17:54:56 +0530

    crypto js

--- utils/cryptoUtils.js
+++ utils/cryptoUtils.js
@@ -4,7 +4,8 @@ const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 const IV_LENGTH = 16;
 
 function encrypt(text) {
-  const iv = crypto.randomBytes(IV_LENGTH);
+  // const iv = crypto.randomBytes(IV_LENGTH);
+  var iv = iv.toString('hex').slice(0, 16);
   const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
   let encrypted = cipher.update(text, 'utf8', 'hex');
   encrypted += cipher.final('hex');
